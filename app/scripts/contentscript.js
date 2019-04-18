0602532b41638ed90a7284ebf5cbf5a67c525098
Rômulo Souza <romulovinicius10@gmail.com>
2019-04-18 15:17:48 -0300

    Added echarts module and injected 2d graph in github page
    Signed-off-by: Rômulo Souza <romulovinicius10@gmail.com>
    Co-authored-by: Cleber Junior <cjj.castro3@gmail.com>

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,43 @@
-console.log(`'Allo 'Allo! Content script`)
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/line';
+import 'echarts/lib/component/tooltip';
+import graph from './graphs.js';
+
+const http = new XMLHttpRequest()
+const url = 'https://reqres.in/api/user?page=2'
+
+const windowurl = window.location.pathname
+console.log('my windowurl = ' + windowurl)
+
+window.addEventListener("load", function load(event){
+    http.open('GET',url)
+    http.send()
+}) 
+
+http.onloadend = ((e) => {
+    console.log('My response = ' + http.responseText)
+})
+
+var node = document.createElement('div')
+var content = document.getElementsByClassName("new-discussion-timeline experiment-repo-nav")
+var repoContent = document.getElementsByClassName("repository-content")
+
+node.innerHTML = graph()
+content[0].insertBefore(node, repoContent[0])
+var myChart = echarts.init(document.getElementById('my-graph'))
+var option = {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+    }]
+}
+myChart.setOption(option)
+
+
---
+++ app/scripts/graphs.js
@@ -0,0 +1,4 @@
+
+export default () => [
+    '<div style="width:600px; height:400px;" id="my-graph"></div>',
+].join('');
