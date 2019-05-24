963fbc2417dac985642a2a3bd99fb0fa795fa29c
Vitor Alves <vitor.alves07750@outlook.com>
2019-05-24 20:27:07 -0300

    Content script tests were excluded due to incompatibility of jest version with js version
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Brian2397 <brianlui2387@gmail.com>
    Co-authored-by: cjj castro <cjj.castro3@gmail.com>

--- app/scripts/__tests__/contentscript.test.js
+++
@@ -1,7 +0,0 @@
-const contentscript = require('app/scripts/contentscript.js')
-
-test1('hello moto', () => {
-    it('should return the right value', () => {
-        expect(contentscript.saveClass('repository-content')).toBe('HTML.collection[div.repository.content]')
-    })
-});
