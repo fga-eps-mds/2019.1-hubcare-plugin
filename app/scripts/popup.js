d0ef19217ebb16e3c15f382b79ab56c016b86c5a
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2019-05-23 22:43:14 -0300

    Created the function that return something when the switch button is used.
    Signed-off-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
    Co-authored-by: Francisco <heronildo.2010@hotmail.com>
    Co-authored-by: Hiroshi <lucas.hh@hotmail.com>

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,24 @@
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/line';
+import 'echarts/lib/component/tooltip';
+import graph from './graphs.js';
+import badges from './badges.js';
+import badge from 'project-badge/dist/badge.js';
+import button from './button.js';
+import loading from './loading.js'
+
 console.log(`'Allo 'Allo! Popup`)
+
+// Pure JS:
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("click-this").addEventListener("click", handler);
+});
+  
+// The handler also must go in a .js file
+function handler() {
+/* ... */
+    console.log(`Test message for plug-in switch button`);
+    //var loading_child = document.getElementById('loading');
+    //Removes the loading object after loading badges
+    //loading_child.parentNode.removeChild(loading_child);
+}
