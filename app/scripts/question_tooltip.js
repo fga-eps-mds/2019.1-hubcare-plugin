ee0e05bad77064907eebcfe8ae55d5d229adabc8
brian2397 <brianlui2387@gmail.com>
2019-06-04 22:08:39 -0300

    The style of the toolTip has been changed. The background of the toolTip was black. And the distance was changed to stay to the right
    Signed-off-by: brian2397 <brianlui2387@gmail.com>

--- app/scripts/question_tooltip.js
+++ app/scripts/question_tooltip.js
@@ -1,9 +1,9 @@
 export default () => [
     '<style>',
     '.tooltip {position: relative; display: inline-block;}',
-    '.tooltip .tooltiptext {visibility: hidden; width: 200px; background-color:rgb(41, 0, 102, 0.99);',
+    '.tooltip .tooltiptext {visibility: hidden; width: 200px; background-color:rgb(69, 69, 69, 0.99);',
     'color: #fff; text-align: center; border-radius: 6px; padding: 5px 0; position:',
-    'absolute; z-index: 1; bottom: 125%; left: 50%; margin-left: -60px; opacity: 0;',
+    'absolute; z-index: 1; bottom: 200%; left: 200%; margin-left: -60px; opacity: 0;',
     'transition: opacity 0.3s;}',
     '.tooltip .tooltiptext::after {position: absolute; -index: 1; bottom: 100%; left: 50%; margin-left: -60px;',
     'border-width: 5px; border-style: solid; border-color: #555 transparent transparent transparent;}',
