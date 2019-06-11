501556b5927f3032556835548d1ddc4981ce7024
Vitor Alves <vitor.alves07750@outlook.com>
2019-06-10 22:26:11 -0300

    Created tests of progress function bar issue and progress bar function
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Francisco <heronildo.2010@hotmail.com>
    Co-authored-by: Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>

---
+++ app/scripts/__tests__/progressbarissue.test.js
@@ -0,0 +1,26 @@
+import progressbarissue from '../progressbarissue.js';
+
+test('test the progress bar issue', async () => {
+    let teste_progressbarissue = [
+    '<style>',
+    '.progress-bar{',
+    'width: 400px;',
+    'height: 30px;',
+    'background-color: red;',
+    'display: flex;',
+    'border-style: solid;',
+    'border-color: black;',
+    'border-width: 1px;',
+    ' border-radius: 5px;}',
+    '.progress-bar::before{',
+    'content: "";',
+    'width: calc(var(--progress) * 1%);',
+    'background-color: #00a000; }',
+    '</style>',
+    '<div style="width:400px; height:75px;"><h1 style="text-align: center;">Issue</h1>',
+    '<div id="bar"></div>',
+    '<div class="progress-bar" ></div>',
+    '</div>',
+    ].join('');
+    expect(progressbarissue()).toBe(teste_progressbarissue);
+});
