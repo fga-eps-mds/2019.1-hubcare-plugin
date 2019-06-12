6bbdc13597d32ecf7893aba7de836405d9777726
Vitor Alves <vitor.alves07750@outlook.com>
2019-06-11 22:24:39 -0300

    Added progress bar test, progress bar issue and support page
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Cjj castro <cjj.castro3@gmail.com>
    Co-authored-by: Brian Luiz <brianlui2387@gmail.com>

---
+++ app/scripts/__tests__/supportPage.test.js
@@ -0,0 +1,46 @@
+import supportPage from '../supportPage.js';
+
+test('test support page function', async () => {
+    let testSupportPage = [
+        '<style>',
+    '.issue-activity{',
+    '   height:120px;',
+    '   margin-left:30%;',
+    '}',
+    '.block{',
+    '   border-top:1px solid #d1d5da;',
+    '}',
+    '.left-element{',
+    '   height:180px;',
+    '   width: 50%;',
+    '   border-right:1px solid #d1d5da;',
+    '   display: inline-block;',
+    '   margin: -1px -1px 0;',
+    '   padding: 8px;',
+    '}',
+    '.right-element{',
+    '   height:180px;',
+    '   width: 50%;',
+    '   display: inline-block;',
+    '   margin: -1px -1px 0;',
+    '   padding: 8px;',
+    '}',
+    '</style>',
+    '<div id="issue-activity" class="issue-activity"></div>',
+    '<div id="issue-activity-rate" class="issue-activity"></div>',
+    '<div class="block">',
+    '   <div id="release-note" class="left-element"></div>',
+    '   <div id="license" class="right-element"></div>',
+    '</div>',
+    '<div class="block">',
+    '   <div id="readme" class="left-element"></div>',
+    '   <div id="code-conduct" class="right-element"></div>',
+    '</div>',
+    '<div class="block">',
+    '   <div id="issue-template" class="left-element"></div>',
+    '   <div id="description" class="right-element"></div>',
+    '</div>',
+    ].join('');
+
+    expect(supportPage()).toBe(testSupportPage);
+});
