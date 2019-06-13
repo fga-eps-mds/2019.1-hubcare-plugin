918e6fd5864ae0e55425a9742e310b725cb3159b
Vitor Alves <vitor.alves07750@outlook.com>
2019-06-12 22:09:00 -0300

    was added from progressbarfunction, graph and welcoming page tests
    Signed-off-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Young Brian <brianlui2387@gmail.com>

--- app/scripts/__tests__/graphs.test.js
+++ app/scripts/__tests__/graphs.test.js
@@ -1,6 +1,8 @@
 import graphs from '../graphs';
 
 test('test graph function', async () => {
-    let test_graph = '<div style="width:600px; height:400px;" id="my-graph"></div>';
+    let test_graph = [
+        '<div style="width: 100%; height:400px;" id="my-graph"></div>',
+    ].join('');
     expect(graphs()).toBe(test_graph);
 })
